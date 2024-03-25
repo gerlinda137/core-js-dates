a4f1b8664b579ddc81410e889b84c10fc7368f52
Angelina Baranova <46976877+gerlinda137@users.noreply.github.com>
2024-03-26 01:00:12 +0400

    feat: update

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -279,8 +279,15 @@ function getWorkSchedule(/* period, countWorkDays, countOffDays */) {
  * Date(2022, 2, 1) => false
  * Date(2020, 2, 1) => true
  */
-function isLeapYear(/* date */) {
-  throw new Error('Not implemented');
+function isLeapYear(date) {
+  const year = new Date(date).getFullYear();
+  if (year % 4 === 0 && year % 100 !== 0) {
+    return true;
+  }
+  if (year % 400 === 0) {
+    return true;
+  }
+  return false;
 }
 
 module.exports = {
